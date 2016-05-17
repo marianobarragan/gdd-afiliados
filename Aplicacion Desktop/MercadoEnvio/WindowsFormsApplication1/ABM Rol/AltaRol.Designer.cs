195bab819b0679d68c899b9bd36cebb817452d8e
agusdefe <agustindefelice17@gmail.com>
2016-05-16 23:24:40 -0300

    LLENALA

--- Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/ABM Rol/AltaRol.Designer.cs
+++ Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/ABM Rol/AltaRol.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(91, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Boton para llenar la cancha";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "ABM Rol";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
