e56e88e8149b0eacb48d91e63b0478c7747d57bb
marianobarragan <mbarragan@est.frba.utn.edu.ar>
2016-06-12 02:43:23 -0300

    Esto ahora funciona de 1000

---
+++ Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/Domain/Rubro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MercadoEnvio.Domain
+{
+    public class Rubro
+    {
+
+        public uint rubro_id;
+        public string descripcion_corta;
+
+        public Rubro(string rubro_texto, string text){
+            rubro_id = UInt32.Parse(rubro_texto);
+            descripcion_corta = text;
+        }
+    }
+}
