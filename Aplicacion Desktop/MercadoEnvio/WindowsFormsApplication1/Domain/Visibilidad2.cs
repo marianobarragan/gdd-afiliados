9a0360bcd7d96571273b42d3883f6ce07dc4f03b
marianobarragan <mbarragan@est.frba.utn.edu.ar>
2016-06-14 17:42:47 -0300

    drogadicto

---
+++ Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/Domain/Visibilidad2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MercadoEnvio.Domain
+{
+    public class Visibilidad2
+    {
+        public string descripcion;
+        public int precio;
+        public int porcentaje;
+        public int costo_envio;
+        public int precioDecimal;
+        public int costo_envioDecimal;
+        public int id;
+
+        public Visibilidad2 (string descripcionNueva, string precioNuevo, string porcentajeNuevo, string costo_envioNuevo,string id)
+        {
+            string[] datos = precioNuevo.Split(',');
+            string[] datos2 = costo_envioNuevo.Split(',');
+            string[] datos3 = porcentajeNuevo.Split(',');
+            descripcion = descripcionNueva;
+            precio = Int32.Parse(datos[0]);
+            porcentaje = Int32.Parse(datos3[1]);
+            costo_envio = Int32.Parse(datos2[0]);
+            precioDecimal = Int32.Parse(datos[1]);
+            costo_envioDecimal = Int32.Parse(datos2[1]);
+
+        }
+
+
+    }
+}
